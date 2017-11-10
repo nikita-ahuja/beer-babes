dd062e7813ee512c10f9d4d4b3886e3690c1d813
Diem McLaughlin <email": null>
2017-11-10 08:39:35 -0600

    took notes about accessing beer by name and id

--- db/migrate/20171109234245_create_beers.rb
+++ db/migrate/20171109234245_create_beers.rb
@@ -2,10 +2,17 @@ class CreateBeers < ActiveRecord::Migration[5.1]
   def change
     create_table :beers do |t|
     	t.string :name
-    	t.integer :api_id
+    	t.string :api_id
     	t.boolean :independent
 
       t.timestamps
     end
   end
 end
+
+
+# find by id
+# b.beers.find('vYlBZQ') --- id
+
+# find by name
+# b.beers.all(name: "Naughty 90").first
