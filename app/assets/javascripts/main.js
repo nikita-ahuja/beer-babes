f0eafc5e646d0cf3a6d56500f1c8c4f05347e505
Nikita Ahuja <email": null>
2017-11-11 11:59:18 -0600

    Add beer form will show and hide successfully

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -1,1 +1,14 @@
 // new WOW().init();
+$(document).ready(function() {
+
+
+$(".add-beer-form").hide()
+$(".beer-form-button").on("click", function(e){
+  e.preventDefault();
+  $(".add-beer-form").toggle();
+
+})
+
+
+
+})
