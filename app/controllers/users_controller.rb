d5ff55ce865cfb890513bbd0f0130e8be5ce1f03
Diem Hoang <hoangd5@gmail.com>
2017-11-12 13:31:43 -0600

    logic and display suggestions based on similar users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,17 +4,30 @@ class UsersController < ApplicationController
   end
 
   def show
-    @current_user = User.find(3)
+    @current_user = User.find(10)
     @suggestions = []
-    @current_user.reviews.each do |review|
-      if review.rating >= 4
-        Beer.where(id: review.beer_id).each do |beer|
-          @suggestions << Beer.find_by(style: beer.style)
+      @current_user.reviews.each do |review|
+        if review.rating >= 3
+          Beer.where(id: review.beer_id).each do |beer|
+            @suggestions << Beer.find_by(style: beer.style)
+          end
         end
       end
-    end
     @suggestions.uniq!
 
+    users = User.where(["light_rating = ? or hoppy_rating = ? or dark_rating = ?", @current_user.light_rating, @current_user.hoppy_rating, @current_user.dark_rating])
+
+    @similar_users = users.reject { |user| user.username == @current_user.username }
+
+    @babe_suggestions = []
+      @similar_users.each do |user|
+        user.reviews.each do |review|
+          if review.rating >= 4
+            @babe_suggestions << Beer.find(review.beer_id)
+          end
+        end
+      end
+    @babe_suggestions.uniq!
   end
 
   def create
