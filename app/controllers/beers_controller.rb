d8825918f9afc6797bfb22ab5d58123f40e72c32
Diem McLaughlin <email": null>
2017-11-12 17:07:08 -0600

    create beer show page

--- app/controllers/beers_controller.rb
+++ app/controllers/beers_controller.rb
@@ -1,11 +1,16 @@
 class BeersController < ApplicationController
 
-   def index
-      if params[:user_id]
-        @beers = User.find(params[:user_id]).beers
-      else
-        @searched_beers = Beer.search(params[:search])
-      end
-    end 
+  def index
+    if params[:user_id]
+      @beers = User.find(params[:user_id]).beers
+    else
+      @searched_beers = Beer.search(params[:search])
+    end
+  end
+
+  def show
+    @beer = Beer.find(params[:id])
+  end
+
 
 end
