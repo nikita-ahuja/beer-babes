241db76e34a886e691813fabc8e4f42f71a95bc1
Diem Hoang <hoangd5@gmail.com>
2017-11-12 07:36:08 -0600

    suggestions based on reviews

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,6 +4,17 @@ class UsersController < ApplicationController
   end
 
   def show
+    @current_user = User.find(3)
+    @suggestions = []
+    @current_user.reviews.each do |review|
+      if review.rating >= 4
+        Beer.where(id: review.beer_id).each do |beer|
+          @suggestions << Beer.find_by(style: beer.style)
+        end
+      end
+    end
+    @suggestions.uniq!
+
   end
 
   def create
