fee160c37a38f738cd754f7a70d591301db82e7e
Diem McLaughlin <email": null>
2017-11-14 11:44:22 -0600

    add validation on user model to require pw length to be min of 6 chars

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,7 @@ class User < ApplicationRecord
 
   validates :username, :email, :password, :light_rating, :dark_rating, :hoppy_rating, presence: true
 
+  validates :password, length: { minimum: 6 }
   validates_uniqueness_of :username, :email, :case_sensitive => false
 
   has_secure_password
