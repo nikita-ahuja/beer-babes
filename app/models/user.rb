e46af456ed8bda6ddc31bff6481e3612168ca913
Diem McLaughlin <email": null>
2017-11-14 16:42:21 -0600

    add current password to authenticate user before saving edit profile

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
   validates :username, :email, :password, :light_rating, :dark_rating, :hoppy_rating, presence: true
 
   validates :password, length: { minimum: 6 }
-  validates_uniqueness_of :username, :email, :case_sensitive => false
+  validates_uniqueness_of :email, :case_sensitive => false
 
   has_secure_password
 end
