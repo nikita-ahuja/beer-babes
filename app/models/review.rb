2dda94a50a73c4e5e5d3a6541fe11a33e7a82e33
Ines Feria <email": null>
2017-11-13 18:14:27 -0600

    Add create a new Shiner Holiday

--- app/models/review.rb
+++ app/models/review.rb
@@ -2,7 +2,7 @@ class Review < ApplicationRecord
 	belongs_to :beer
   belongs_to :user
 
-  validates :user_id, :beer_id, :rating, presence: true
+  validates :user_id, :rating, presence: true
 
   validates :user_id, uniqueness: {scope: [:beer_id]}
   # validates_uniqueness_of :user_id, :scope => :beer_id
