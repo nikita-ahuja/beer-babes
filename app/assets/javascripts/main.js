731d572bc6f0ea12f94a08cb3b9e8b128fa78715
nikita ahuja <nahuja038@gmail.com>
2017-11-16 10:20:53 -0600

    Add JS stuff

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -1,10 +1,11 @@
 $(document).ready(function() {
-
+https://github.com/Aus-Red-Pandas-2017/phase-3-guide
 $(".users.new").ready(function () {
   $('#myModal').modal('show');
   $(".btn").on('click',function(){
     $('#myModal').modal('hide');
   })
+
 });
 
 
@@ -12,6 +13,21 @@ $(".add-beer-form").hide()
 $(".beer-form-button").on("click", function(e){
   e.preventDefault();
   $(".add-beer-form").toggle();
+
+// $(document).keypress(function(e){
+// $(".beer-form-button").on("click", function(e){
+//   e.preventDefault();
+//   $(".add-beer-form").toggle();
+
+$(document).on('keyup',(function(e){
+  if (e.which == 13){
+    $(".add-beer-form").toggle();
+  }
+
+});
+
+
+});
 
 });
 
